5ff3ab918572d7e90c8ec6405cc14871f1650e5a
Jakub Flaska <jakub.flaska@nih.gov>
2018-01-05 12:51:16 -0500

    good score

--- openai/generalAlgRL/main.py
+++ openai/generalAlgRL/main.py
@@ -15,7 +15,7 @@ env = gym.make('CartPole-v0')
 input_size = 4
 goal_steps = 500
 score_requirement = 50
-initial_games = 10000
+initial_games = 1000
 
 def transform_actions(training_data, game_memory):
 	for data in game_memory:
