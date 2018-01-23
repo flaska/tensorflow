004fd6ae7f2b7a58be1b17dbc7f2510713e7fe03
Jakub Flaska <jakub.flaska@gmail.com>
2018-01-22 20:16:49 -0500

    done

--- LinReg/linreg.py
+++ LinReg/linreg.py
@@ -5,16 +5,16 @@ import network
 
 X = [30,40,40,50,50,50,60,70,70,80,80,80]
 Y = [70,90,100,120,130,150,160,190,200,200,220,230]
-#Y = [1,1,1,1,1,1,1,1,1,1,1,1]
 
-X = numpy.array(X).reshape(12, 1)
-Y = numpy.array(Y).reshape(12,1)
 
 network = network.get_network_spec()
 model = tflearn.DNN(network, tensorboard_dir='log')
 
-model.fit({'input':X}, {'targets':Y}, n_epoch=5, snapshot_step=500, show_metric=False, run_id='openaistuff')	
+model.fit(X, Y, n_epoch=1000, show_metric=True)	
 
+prediction = model.predict([30,40,50,60,70,80])
+print("\n\nPredticion")
+print(prediction)
 
 
 	
--- LinReg/network.py
+++ LinReg/network.py
@@ -1,24 +1,14 @@
 import tflearn
- 
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
-
-LR = 1e-3
 
 def get_network_spec():
-	network = input_data(shape=[None, 1], name='input')
+	network = tflearn.input_data(shape=[None])
+	network = tflearn.single_unit(network)
+	network = tflearn.regression(network, optimizer='sgd', loss='mean_square', metric='R2', learning_rate=0.01)	
+	return network
 
-	network = fully_connected(network, 64, activation='relu')
-	network = dropout(network, 0.8)
-	
-	network = fully_connected(network, 128, activation='relu')
-	network = dropout(network, 0.8)
-	
-	network = fully_connected(network, 64, activation='relu')
-	network = dropout(network, 0.8)	
-	
-	network = fully_connected(network, 1, activation='softmax')
-	
-	network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
-	
+
+def get_network_spec():
+	network = tflearn.input_data(shape=[None])
+	network = tflearn.single_unit(network)
+	network = tflearn.regression(network, optimizer='sgd', loss='mean_square', metric='R2', learning_rate=0.01)	
 	return network
