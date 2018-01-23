98349538ffbd32e9ee01bf1ef183dfa843979816
Jakub Flaska <jakub.flaska@gmail.com>
2018-01-22 21:10:14 -0500

    done

--- LinReg/network.py
+++ LinReg/network.py
@@ -5,10 +5,3 @@ def get_network_spec():
 	network = tflearn.single_unit(network)
 	network = tflearn.regression(network, optimizer='sgd', loss='mean_square', metric='R2', learning_rate=0.01)	
 	return network
-
-
-def get_network_spec():
-	network = tflearn.input_data(shape=[None])
-	network = tflearn.single_unit(network)
-	network = tflearn.regression(network, optimizer='sgd', loss='mean_square', metric='R2', learning_rate=0.01)	
-	return network
