8e75db7355eb623f7892a4e66816acc25bfef8a3
Jakub Flaska <jakub.flaska@nih.gov>
2018-02-28 13:29:16 -0500

    conv working

--- tflearn/mnist_conv/conv.py
+++ tflearn/mnist_conv/conv.py
@@ -8,28 +8,35 @@ from tflearn.layers.estimator import regression
 
 # Data loading and preprocessing
 import tflearn.datasets.mnist as mnist
+import numpy
 X, Y, testX, testY = mnist.load_data(one_hot=True)
+
+X = numpy.split(X,4)[0]
+Y = numpy.split(Y,4)[0]
+
 X = X.reshape([-1, 28, 28, 1])
 testX = testX.reshape([-1, 28, 28, 1])
 
 # Building convolutional network
 network = input_data(shape=[None, 28, 28, 1], name='input')
-network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
+network = conv_2d(network, 8, 3, activation='relu', regularizer="L2")
 network = max_pool_2d(network, 2)
 network = local_response_normalization(network)
-network = conv_2d(network, 64, 3, activation='relu', regularizer="L2")
+network = conv_2d(network, 16, 3, activation='relu', regularizer="L2")
 network = max_pool_2d(network, 2)
 network = local_response_normalization(network)
-network = fully_connected(network, 128, activation='tanh')
+network = fully_connected(network, 32, activation='tanh')
 network = dropout(network, 0.8)
-network = fully_connected(network, 256, activation='tanh')
+network = fully_connected(network, 64, activation='tanh')
 network = dropout(network, 0.8)
 network = fully_connected(network, 10, activation='softmax')
-network = regression(network, optimizer='adam', learning_rate=0.01,
+network = regression(network, optimizer='adam', learning_rate=0.02,
                      loss='categorical_crossentropy', name='target')
 
 # Training
 model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit({'input': X}, {'target': Y}, n_epoch=20,
+model.fit({'input': X}, {'target': Y}, n_epoch=5,
            validation_set=({'input': testX}, {'target': testY}),
            snapshot_step=100, show_metric=True, run_id='convnet_mnist')
+		   
+model.save('./conv.model')
